aacb8722cf8baeb81c49470d657d6ee688c0fe6a
Denis Faini <dtf8841@rit.edu>
2025-07-18 16:22:57 -0400

    Fix maven bug and added comments

--- Windows/setup_window.ps1
+++ Windows/setup_window.ps1
@@ -18,46 +18,47 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
 # Install all the packages
 
 # Browsers
-winget install --id Mozilla.Firefox -e
-
-# Drive
-winget install --id Google.GoogleDrive -e
-
-# Development Tools
-winget install --id WiresharkFoundation.Wireshark -e
-winget install --id Python.Python.$pythonVersion -e
-
-# Git
-winget install --id Git.Git -e
-
-# Text Editors / IDEs / Readers
-winget install --id Adobe.Acrobat.Reader.64-bit -e
-winget install --id Microsoft.VisualStudioCode -e
-winget install --id Mobatek.MobaXterm -e
-
-# Media
-winget install --id VideoLAN.VLC -e
-
-# Utilities + Other
-winget install --id 7zip.7zip -e
-winget install --id PuTTY.PuTTY -e
-winget install --id Notepad++.Notepad++ -e
-winget install --id Tailscale.Tailscale -e
-winget install --id Greenshot.Greenshot -e
-winget install --id Oracle.JDK.$javaVersion  -e
-winget install --id WinFsp.WinFsp -e
-
-# Office
-# winget install --id Microsoft.Office.ProPlus -e
-winget install --id Microsoft.Office365 -e
+winget install --id Mozilla.Firefox -e                      # Firefox
+winget install --id Google.GoogleDrive -e                   # Google Drive
 
 # Python Packages
 # Install Python packages using pip
-pip install pytest
+pip install pytest                                          # Pytest for unit test
+
+# Development Tools
+winget install --id WiresharkFoundation.Wireshark -e        # Network Protocol Analyzer
+winget install --id Python.Python.$pythonVersion -e         # Python 3.x
+winget install --id Microsoft.WindowsSubsystemForLinux -e   # Windows Subsystem for Linux
+
+# Git
+winget install --id Git.Git -e                              # Git Version Control 
+
+# Media
+winget install --id VideoLAN.VLC -e                         # VLC Media Player
+winget install --id MusicBrainz.Picard -e                   # MusicBrainz Picard
+
+# Utilities + Other
+winget install --id 7zip.7zip -e                             # 7-Zip File Archiver
+winget install --id PuTTY.PuTTY -e                           # PuTTY
+winget install --id Notepad++.Notepad++ -e                   # Notepad++
+winget install --id Tailscale.Tailscale -e                   # Tailscale
+winget install --id Greenshot.Greenshot -e                   # Greenshot
+winget install --id Oracle.JDK.$javaVersion  -e              # Oracle JDK
+winget install --id WinFsp.WinFsp -e                         # WinFsp
+winget install --id MullvadVPN.MullvadVPN -e                # Mullvad VPN
+
+# Office
+# winget install --id Microsoft.Office.ProPlus -e
+winget install --id Microsoft.Office365 -e                  # Microsoft Office 365
+
+# Text Editors / IDEs / Readers
+winget install --id Microsoft.VisualStudioCode -e            # Visual Studio Code
+winget install --id Mobatek.MobaXterm -e                     # MobaXterm
+winget install --id Adobe.Acrobat.Reader.64-bit -e           # Adobe Acrobat Reader
 
 # Configure Git Global Settings
-git config --global user.name "$gitUsername"
-git config --global user.email "$gitEmail"
+git config --global user.name "$gitUsername"         # Set Git username
+git config --global user.email "$gitEmail"           # Set Git email
 
 # Download maven
 Invoke-WebRequest -Uri "https://dlcdn.apache.org/maven/maven-3/$mavenVersion/binaries/apache-maven-$mavenVersion-bin.zip" -OutFile "C:\apache-maven-$mavenVersion-bin.zip"
@@ -69,8 +70,14 @@ Expand-Archive -Path "C:\apache-maven-$mavenVersion-bin.zip" -DestinationPath "C
 [System.Environment]::SetEnvironmentVariable("MAVEN_HOME", "C:\Program Files\Apache\maven\apache-maven-$mavenVersion", [System.EnvironmentVariableTarget]::Machine)
 
 # Update PATH environment variable
-$env:Path += ";$env:MAVEN_HOME\bin"
+$env:Path += ";C:\Program Files\Apache\maven\apache-maven-$mavenVersion\bin"
 [System.Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
 
+# Refresh PATH in current session
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+
+# Delete the maven zip file from the system
+Remove-Item -Path "C:\apache-maven-$mavenVersion-bin.zip" -Force
+
 # Microsoft Activation Scripts (MAS)
-irm https://massgrave.dev/get | iex # manually run this script to activate Windows
+irm https://get.activated.win | iex # manually run this script to activate Windows
