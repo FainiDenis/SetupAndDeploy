519d41839b049aab8ef4c2b93d411992945b3c1a
Denis Faini <dtf8841@rit.edu>
2025-07-18 17:31:59 -0400

    Refactor Windows setup script to use functions

--- Windows/setup_window.ps1
+++ Windows/setup_window.ps1
@@ -8,76 +8,91 @@ $javaVersion = "24"
 $gitUsername = "FainiDenis"
 $gitEmail = "dtf8841@rit.edu"
 
-
-# Check if winget is installed
-if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    Write-Host "winget is not installed. Please install the App Installer from the Microsoft Store."
-    exit
+# function to check if a command exists
+function check_exists_command {
+    param (
+        [string]$command
+    )
+    if (-not (Get-Command $command -ErrorAction SilentlyContinue)) {
+        Write-Host "$command is not installed. Please install it."
+        exit
+    }
 }
 
-# Install all the packages
+# function to install packages using winget
+function install_packages {
+    param (
+        [string[]]$packages
+    )
+    foreach ($package in $packages) {
+        winget install --id $package -e
+    }
+}
 
-# Browsers
-winget install --id Mozilla.Firefox -e                      # Firefox
-winget install --id Google.GoogleDrive -e                   # Google Drive
+# function to configure Git
+function configure-git {
+    git config --global user.name $gitUsername
+    git config --global user.email $gitEmail
+}
 
-# Python Packages
-# Install Python packages using pip
-pip install pytest                                          # Pytest for unit test
+# function to download and set up Maven
+function setup-maven {
+    $mavenUrl = "https://dlcdn.apache.org/maven/maven-3/$mavenVersion/binaries/apache-maven-$mavenVersion-bin.zip"
+    $mavenZipPath = "C:\apache-maven-$mavenVersion-bin.zip"
+    $mavenInstallPath = "C:\Program Files\Apache\maven"
 
-# Development Tools
-winget install --id WiresharkFoundation.Wireshark -e        # Network Protocol Analyzer
-winget install --id Python.Python.$pythonVersion -e         # Python 3.x
-winget install --id Microsoft.WindowsSubsystemForLinux -e   # Windows Subsystem for Linux
+    Invoke-WebRequest -Uri $mavenUrl -OutFile $mavenZipPath
+    Expand-Archive -Path $mavenZipPath -DestinationPath $mavenInstallPath
 
-# Git
-winget install --id Git.Git -e                              # Git Version Control 
+    # Set environment variables for Maven
+    [System.Environment]::SetEnvironmentVariable("MAVEN_HOME", "$mavenInstallPath\apache-maven-$mavenVersion", [System.EnvironmentVariableTarget]::Machine)
+    $env:Path += ";$mavenInstallPath\apache-maven-$mavenVersion\bin"
+    [System.Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
 
-# Media
-winget install --id VideoLAN.VLC -e                         # VLC Media Player
-winget install --id MusicBrainz.Picard -e                   # MusicBrainz Picard
+    # Refresh PATH in current session
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
 
-# Utilities + Other
-winget install --id 7zip.7zip -e                             # 7-Zip File Archiver
-winget install --id PuTTY.PuTTY -e                           # PuTTY
-winget install --id Notepad++.Notepad++ -e                   # Notepad++
-winget install --id Tailscale.Tailscale -e                   # Tailscale
-winget install --id Greenshot.Greenshot -e                   # Greenshot
-winget install --id Oracle.JDK.$javaVersion  -e              # Oracle JDK
-winget install --id WinFsp.WinFsp -e                         # WinFsp
-winget install --id MullvadVPN.MullvadVPN -e                # Mullvad VPN
+    # Clean up
+    Remove-Item -Path $mavenZipPath -Force
+}
 
-# Office
-# winget install --id Microsoft.Office.ProPlus -e
-winget install --id Microsoft.Office365 -e                  # Microsoft Office 365
+# main script execution starts here
 
-# Text Editors / IDEs / Readers
-winget install --id Microsoft.VisualStudioCode -e            # Visual Studio Code
-winget install --id Mobatek.MobaXterm -e                     # MobaXterm
-winget install --id Adobe.Acrobat.Reader.64-bit -e           # Adobe Acrobat Reader
+# check if winget is installed
+check_exists_command -command "winget"
 
-# Configure Git Global Settings
-git config --global user.name "$gitUsername"         # Set Git username
-git config --global user.email "$gitEmail"           # Set Git email
+# define the list of packages to install
+$wingetPackages = @(
+    "Mozilla.Firefox",                      # Firefox browser
+    "WiresharkFoundation.Wireshark",        # Wireshark
+    "Python.Python.$pythonVersion",         # Python
+    "Microsoft.WindowsSubsystemForLinux",   # WSL
+    "Git.Git",                              # Git
+    "VideoLAN.VLC",                         # VLC Media Player
+    "MusicBrainz.Picard",                   # MusicBrainz Picard
+    "7zip.7zip",                            # 7-Zip
+    "PuTTY.PuTTY",                          # PuTTY
+    "Notepad++.Notepad++",                  # Notepad++
+    "Tailscale.Tailscale",                  # Tailscale
+    "Greenshot.Greenshot",                  # Greenshot
+    "Oracle.JDK.$javaVersion",              # Oracle JDK
+    "WinFsp.WinFsp",                        # WinFsp
+    "MullvadVPN.MullvadVPN",                # Mullvad VPN
+    "Microsoft.Office365",                  # Microsoft Office 365
+    "Microsoft.VisualStudioCode",           # Visual Studio Code
+    "Mobatek.MobaXterm",                    # MobaXterm
+    "Google.GoogleDrive",                   # Google Drive
+    "Adobe.Acrobat.Reader.64-bit"           # Adobe Acrobat Reader
+)
 
-# Download maven
-Invoke-WebRequest -Uri "https://dlcdn.apache.org/maven/maven-3/$mavenVersion/binaries/apache-maven-$mavenVersion-bin.zip" -OutFile "C:\apache-maven-$mavenVersion-bin.zip"
+# call the function to install packages
+install_packages -packages $wingetPackages
 
-# Extract maven
-Expand-Archive -Path "C:\apache-maven-$mavenVersion-bin.zip" -DestinationPath "C:\Program Files\Apache\maven"
+# call the function to configure Git
+configure-git
 
-# Set environment variables for maven
-[System.Environment]::SetEnvironmentVariable("MAVEN_HOME", "C:\Program Files\Apache\maven\apache-maven-$mavenVersion", [System.EnvironmentVariableTarget]::Machine)
-
-# Update PATH environment variable
-$env:Path += ";C:\Program Files\Apache\maven\apache-maven-$mavenVersion\bin"
-[System.Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
-
-# Refresh PATH in current session
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
-
-# Delete the maven zip file from the system
-Remove-Item -Path "C:\apache-maven-$mavenVersion-bin.zip" -Force
+# call the function to download and set up maven
+setup-maven
 
 # Microsoft Activation Scripts (MAS)
 irm https://get.activated.win | iex # manually run this script to activate Windows
